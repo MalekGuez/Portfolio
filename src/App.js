a692741f9c3370d83b5405c4023054fb83136e26
Malek <rogex2542@gmail.com>
2022-01-20 18:40:13 +0100

    feat: added home and 404 components + some file clearing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import FourOhFour from './pages/FourOhFour';
+import Home from './pages/Home';
 
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<FourOhFour />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
